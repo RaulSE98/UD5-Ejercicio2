4c11b29891d6fc5336b15a6468946e75cbb8d21f
RaulSE98 <56968210+RaulSE98@users.noreply.github.com>
2021-07-07 12:54:01 +0200

    Create Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Raul";
+		
+		JOptionPane.showMessageDialog(null, "Bienvenido " +nombre);
+
+	}
+
+}
